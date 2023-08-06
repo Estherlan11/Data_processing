d7902b3ca199b1ffbd3a549be90b57fda7c017c7
Estherlan11 <114509827+Estherlan11@users.noreply.github.com>
2023-08-07 02:26:15 +0800

    Update modify.py

--- modify.py
+++ modify.py
@@ -1,8 +1,8 @@
+#import the necessary packages
 import pandas as pd
 
-
 # Read cvs file
-df = pd.read_csv('E:\\allmean.csv')
+df = pd.read_csv('E:\\result.csv')
 # extract the first column
 df1 = df.iloc[:, 0]
 # split each row of the first column
@@ -12,5 +12,4 @@ df4 = df3.iloc[:, 0]
 # save the first column with the rest of the data
 dff = pd.concat([df4, df.iloc[:, 1:]], axis=1)
 # save the new csv file
-dff.to_csv('E:\\allm.csv', index=False)
-
+dff.to_csv('E:\\xxx.csv', index=False)
