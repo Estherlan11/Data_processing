f63edfefca1b7de4968822129daa47c310a2525a
Estherlan11 <114509827+Estherlan11@users.noreply.github.com>
2023-08-07 02:22:33 +0800

    Update mean.py

--- mean.py
+++ mean.py
@@ -4,23 +4,24 @@ import arcpy
 from arcpy.sa import *
 
 '''
-功能：
-    计算输入文件夹inws内，所有栅格数据的平均值
+Function:
+    Calculate the average of all raster data in the input folder inws
 '''
 
-arcpy.CheckOutExtension("ImageAnalyst")  # 检查许可
+#check the permission
+arcpy.CheckOutExtension("ImageAnalyst") 
 arcpy.CheckOutExtension("spatial")
 
-# 输入路径  应该注意，中文路径，会导致读不出文件；路径尽量不要有空格，写文件时会报错
+# Input the file paths 
 inpath = r'E:\newdata\hos'
 
 OutputFile = open(inpath + 'mean.csv', 'w')
 
-# 利用glob包，将inws下的所有tif文件读存放到rasters中
+# Use the glob package to read and store all tif files under inws into rasters
 rasters = glob.glob(os.path.join(inpath, "*.tif"))
-whereClause = "VALUE = 0"  # 去除异常值
+whereClause = "VALUE = 0"   # remove outliers
 
-# 循环rasters中的所有影像，进行“求平均值”操作
+# Loop over all the images in rasters and "average" them.
 for ras in rasters:
     arcpy.CalculateStatistics_management(ras)
     meanValueInfo = arcpy.GetRasterProperties_management(ras, 'MEAN')
@@ -29,5 +30,4 @@ for ras in rasters:
     OutputFile.write(os.path.basename(ras) + ',' + str(meanValue) + '\n')
 
 OutputFile.close()
-print("All project is OK！")
-
+print("All project is OK!")
